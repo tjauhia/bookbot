f22f4176c3ecae3747ce7b51cc085f19ddf094dc
Tuomas Jauhiainen <tjauhia@gmail.com>
2024-03-06 20:37:19 +0200

    final function. Count the characters with imported Counter class

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from collections import Counter
+#Could not figure out any other way to count the charactes in the text so I imported this class
 def main():
     path_to_book = "books/frankenstein.txt" #define the path to the book to be used in other functions
     show_text = text_output(path_to_book) #define the use of text_output function. Takes the books path as an input
     num_count = word_counter(show_text) #define the use of word_counter function. Takes the text from text_output function as an input
+    characters = character_count(show_text)
     print(f"We have {num_count} words in this book")#print the word count from word_counter function
+    print("And below we have a count for each character in the text")
+    print(characters)
 #display the contents of the book. Use with-statement block
 def text_output(path_to_book):
     with open(path_to_book) as f:#open the contents and rename the value as f
@@ -11,5 +16,10 @@ def text_output(path_to_book):
 def word_counter(show_text):
     words = show_text.split()
     return len(words)
+#function to count each character in the book
+def character_count(show_text):
+    lower_chrs = show_text.lower()
+    chr_counter = Counter(lower_chrs)
+    return chr_counter
 
 main()
