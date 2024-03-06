ff4751afc29efb0faf24cc5469a04593c60535ab
Tuomas Jauhiainen <tjauhia@gmail.com>
2024-03-06 19:46:17 +0200

    restructuted the code with separate functions

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-#as long as the code base is small, I think I can run this in the main function?
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    path_to_book = "books/frankenstein.txt"
+    show_text = text_output(path_to_book)
+    num_count = word_counter(show_text)
+    print(f"We have {num_count} words in this book")
+
+def text_output(path_to_book):
+    with open(path_to_book) as f:
+        return f.read()
+
+def word_counter(show_text):
+    words = show_text.split()
+    return len(words)
+
 main()
