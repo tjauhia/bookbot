8392687ebfbda6086278ac5a6a1c1bd072617dab
Tuomas Jauhiainen <tjauhia@gmail.com>
2024-03-09 16:06:59 +0200

    Huge restructuring. Couple new functions for final asssesment

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def main():
     path_to_book = "books/frankenstein.txt" #define the path to the book to be used in other functions
     show_text = text_output(path_to_book) #define the use of text_output function. Takes the books path as an input
     num_count = word_counter(show_text) #define the use of word_counter function. Takes the text from text_output function as an input
-    characters = character_count(show_text)
+    characters = character_count(show_text) #dictionary define
+    the_sorted_list = list_counts(characters)
+    list_only_alphabet_char = only_alphabet(the_sorted_list)
     print(f"We have {num_count} words in this book")#print the word count from word_counter function
-    print()
+    print("__Lets start the report__")
+    print(only_alphabet)
+    print("The End")
+
 #display the contents of the book. Use with-statement block
 def text_output(path_to_book):
     with open(path_to_book) as f:#open the contents and rename the value as f
@@ -26,7 +31,25 @@ def character_count(show_text):
             chr_dict[lower_chrs] = 1
     return chr_dict
 
-def list_counts(characters):
+#function for sorting the number part in the list of dicts
+#had to take a peek for this one since I could not figure out how to do this part
+def sorttaus(d):
+   return d["num"]
 
+def only_alphabet(the_sorted_list):
+    alph_list = the_sorted_list
+    for i in alph_list:
+        if not i["char"].isalpha(): #if character is part of the alphabet, print out the char and the count
+            continue
+        print(f"The '{i['char']}' character was found {i['num']} times")
+    return alph_list
+
+def list_counts(characters):
+    sorted_list = []
+    sorted_list.sort(reverse=True,key=characters)
+    for char in characters:
+        sorted_list.append({"char": char, "num": characters[char]})
+    sorted_list.sort(reverse=False, key=sorttaus) #I want to count listing in ascending order
+    return sorted_list
 
 main()
